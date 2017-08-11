0c3b712a30e763e6c6a1112cfa061637acf291f7
linkcoding <kenny.lin.91@gmail.com>
2017-08-11 08:42:09 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,52 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/posts/new' do
+    erb :new
+  end
+
+  patch '/posts/:id' do
+    #binding.pry
+    @post = Post.find(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+
+    redirects "/posts/#{@post.id}"
+  end
+
+  delete '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    erb :delete
+  end
+
+  get '/posts/:id/edit' do
+    #binding.pry
+    @post = Post.find(params[:id])
+    erb :edit
+  end
+  #when patch, include the controller method:override
+
+  get '/posts/:id' do
+    #binding.pry
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  get '/posts' do
+    @post = Post.all
+    erb :posts
+  end
+
+  post '/posts' do
+    @post = Post.new(params)
+    @post.save
+    redirect '/posts'
+  end
+
+  get '/' do
+
   end
 end
